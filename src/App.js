0d41222fb83ccfa971ffc69f50b0c72ac832b11f
sanaeas <sanaassim1@gmail.com>
2023-01-16 11:01:37 +0100

    Add the user to the data layer, add sign out function

--- src/App.js
+++ src/App.js
@@ -3,8 +3,32 @@ import Home from "./Components/Home";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
+import { useStateValue } from "./StateProvider";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log("The user is:", user);
+
+      if (user) {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <Router>
       <div className="App">
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,8 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../images/fash.png";
 import { ShoppingCartIcon, UserIcon } from "@heroicons/react/24/outline";
+import { useStateValue } from "../StateProvider";
+import { signOut } from 'firebase/auth';
+import { auth } from "../firebase";
 
 function Nav() {
+    const [{ user }, dispatch] = useStateValue();
+
+    function signoutBtn() {
+        signOut(auth)
+            .then(() => {
+                console.log('user signed out')
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+    }
+
     return (
         <nav className="py-2 px-3 shadow-md md:px-5">
             <div className="flex justify-between items-center w-full mx-auto max-w-[1200px]">
@@ -27,7 +42,15 @@ function Nav() {
 
                 <div className="flex items-center gap-x-3">
                     <Link to='/' className="flex items-center border border-black rounded-full px-3 py-1"><ShoppingCartIcon className="h-7 mr-2" />Cart</Link>
-                    <Link to='/login'><UserIcon className="h-7" /></Link>
+                    {user ? (
+                        <div className="px-3">
+                            <p className="font-bold">Hello,</p>
+                            <p className="text-sm">{user.email}</p>
+                            <p className="cursor-pointer text-sm text-gray-700" onClick={signoutBtn}>Sign Out</p>
+                        </div>
+                    ) : (
+                        <Link to='/login'><UserIcon className="h-7" /></Link>
+                    )}
                 </div>
             </div>
         </nav>
