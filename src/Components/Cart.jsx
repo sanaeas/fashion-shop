8daa849a44aac36b5f81bb2892074af055e40f6a
sanaeas <sanaassim1@gmail.com>
2023-01-18 17:03:43 +0100

    Go to payment if you click check out

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { useStateValue } from '../StateProvider';
 import CartProduct from './CartProduct';
 
 function Cart() {
     const [{ cart }, dispatch] = useStateValue();
     const [total, setTotal] = useState(0);
+    const navigate = useNavigate()
 
     useEffect(() => {
         let total = cart.reduce(
@@ -19,7 +21,7 @@ function Cart() {
             <div className='bg-white flex justify-between items-center py-3 px-10 rounded-2xl shadow-md mb-9'>
                 <h2 className='text-xl tracking-wider font-light'>Your Shopping Basket</h2>
                 <p>{cart.length} Items</p>
-                <button className='bg-indigo-600 text-white px-4 py-2 rounded-lg'>Check out (${total})</button>
+                <button className='bg-indigo-600 text-white px-4 py-2 rounded-lg' onClick={e => navigate('/payment')}>Check out (${total})</button>
             </div>
 
             <div className='bg-white py-3 px-10 rounded-2xl shadow-md'>
