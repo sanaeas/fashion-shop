3e058ee2afed8b71c1541721b73ed68159c2123f
sanaeas <sanaassim1@gmail.com>
2023-01-18 17:09:37 +0100

    Stripe configuration

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
 import Cart from "./Components/Cart";
 import AllProducts from "./Components/AllProducts";
+import Payment from "./Components/Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe('pk_test_51M8JyEDI382dDOcJ62L4dFaYsFFfN84RC8o5NlCM5blCxqfqafLz9cXo9pkZFMOQt4xD3iFXousDg6jX5CjKva0j00SDkZOfkr');
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -52,6 +57,11 @@ function App() {
           <Route path='/signup' element={<Signup />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/products' element={<AllProducts />} />
+          <Route path='/payment' element={
+                <Elements stripe={promise}>
+                  <Payment />
+                </Elements>
+            } />
         </Routes>
       </div>
     </Router>
