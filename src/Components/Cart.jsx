7a0ef0de0a08c448e2cf69d0c5bdef6d73131efe
sanaeas <sanaassim1@gmail.com>
2023-01-19 09:33:23 +0100

    Disable the checkout button if the user is not logged in

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -4,7 +4,7 @@ import { useStateValue } from '../StateProvider';
 import CartProduct from './CartProduct';
 
 function Cart() {
-    const [{ cart }, dispatch] = useStateValue();
+    const [{ cart, user }, dispatch] = useStateValue();
     const [total, setTotal] = useState(0);
     const navigate = useNavigate()
 
@@ -18,11 +18,12 @@ function Cart() {
   return (
     <div className='bg-gray-100 min-h-screen pt-9 px-3'>
         <div className='max-w-[1200px] mx-auto'>
-            <div className='bg-white flex justify-between items-center py-3 px-10 rounded-2xl shadow-md mb-9'>
+            <div className='bg-white flex justify-between items-center py-3 px-10 rounded-2xl shadow-md'>
                 <h2 className='text-xl tracking-wider font-light'>Your Shopping Basket</h2>
                 <p>{cart.length} Items</p>
-                <button className='bg-indigo-600 text-white px-4 py-2 rounded-lg' onClick={e => navigate('/payment')}>Check out (${total})</button>
+                <button className='bg-indigo-600 text-white px-4 py-2 rounded-lg' disabled={!user} onClick={e => navigate('/payment')}>Check out (${total})</button>
             </div>
+            <p className='mb-9 mt-2 px-3 text-sm font-light text-gray-500 text-right'>To continue with the payment, please make sure you are logged in.</p>
 
             <div className='bg-white py-3 px-10 rounded-2xl shadow-md'>
                 <div className='flex w-full border-b text-gray-700 text-sm py-2'>
