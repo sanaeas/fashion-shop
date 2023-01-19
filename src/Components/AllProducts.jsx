3d2de994dd91dc6888204f35335cb16dd49d6636
sanaeas <sanaassim1@gmail.com>
2023-01-19 16:10:53 +0100

    Change number of grid cols - responsive

--- src/Components/AllProducts.jsx
+++ src/Components/AllProducts.jsx
@@ -24,7 +24,7 @@ function AllProducts() {
     <div>
         <div className='max-w-[1200px] mx-auto'>
             <h2 className='text-center text-4xl font-bold mt-14 mb-4'>Products</h2>
-            <div className='my-10 px-6 flex gap-x-7'>
+            <div className='my-10 px-6 flex flex-wrap gap-x-7 gap-y-5'>
                 <span className='py-2 px-4 border-2 rounded-3xl cursor-pointer text-gray-600' onClick={() => setProds(products)}>All</span>
                 <span className='py-2 px-4 border-2 rounded-3xl cursor-pointer text-gray-600' onClick={() => setCategory("women's clothing")}>Women's clothing</span>
                 <span className='py-2 px-4 border-2 rounded-3xl cursor-pointer text-gray-600' onClick={() => setCategory("men's clothing")}>Men's clothing</span>
@@ -32,7 +32,7 @@ function AllProducts() {
                 <span className='py-2 px-4 border-2 rounded-3xl cursor-pointer text-gray-600' onClick={() => setCategory('electronics')}>Electronics</span>
             </div>
             <div>
-                <div className='w-full grid grid-cols-3 py-4 px-5 gap-x-5 gap-y-11 my-20'>
+                <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 py-4 px-5 gap-x-5 gap-y-11 my-20'>
                     {prods?.map(({id, title, price, image, rating}) => (
                         <SingleProduct key={id} id={id} title={title} price={price} image={image} rating={rating.rate} />
                     ))}
