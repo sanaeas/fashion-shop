681bbd20cb5f4ba2bfdc9833bf41254a6361f8b0
sanaeas <sanaassim1@gmail.com>
2023-01-19 16:16:45 +0100

    Change the make width

--- src/Components/Order.jsx
+++ src/Components/Order.jsx
@@ -5,7 +5,7 @@ import moment from 'moment';
 function Order({ order }) {
     console.log(order)
   return (
-    <div className='mb-6 bg-gray-300 max-w-[1200px] mx-auto py-3 px-5 rounded-xl shadow-lg'>
+    <div className='mb-6 bg-gray-300 max-w-[1000px] mx-auto py-3 px-5 rounded-xl shadow-lg'>
         <div className='flex flex-col items-center justify-center my-4'>
             <h2 className='text-xl mr-4 font-bold text-gray-700 tracking-wide'>Order</h2>
             <p className='text-sm mt-1 font-light'>{moment.unix(order.data.created).format('MMMM Do YYYY, h:mma')}</p>
