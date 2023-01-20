b4ea3f36ca958aa175d840c88f8352e26108ecdb
sanaeas <sanaassim1@gmail.com>
2023-01-20 10:40:27 +0100

    Add the actual API (cloud functions) URL

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: 'https://us-central1-clone-8d036.cloudfunctions.net/api'  // The API (cloud functions) URL
-    baseURL: 'http://localhost:5001/fashion--shop/us-central1/api'
+    baseURL: 'https://us-central1-fashion--shop.cloudfunctions.net/api'  // The API (cloud functions) URL
 });
 
 export default instance;
